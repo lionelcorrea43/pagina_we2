d0c4f1494f99ae1b607dfef06c5b84e2cb95c6c1
Leonel Godoy Chaparro <godoychaparroleonel@gmail.com>
2026-09-17 12:43:24 -0300

    agregue archivo .htacces

--- app/model/model.php
+++ app/model/model.php
@@ -17,7 +17,7 @@ class Model
         $host = 'localhost';
         $userName = 'root';
         $password = '';
-        $database = 'equipo_jugadores';
+        $database = 'futbol';
         try {
             $this->pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $userName, $password);
         } catch (Exception  $e) {
